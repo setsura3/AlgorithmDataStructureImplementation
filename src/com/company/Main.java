881affc2ac836380e380aced883068a67948064d
Mini-Project-MovingCircles <setsura3@gmail.com>
2020-02-02 14:00:52 -0800

    Implementation of Data Structure and Algorithm with Java practice.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BST<Integer> bst = new BST<>();
+        int nums[] = {5, 3,4, 6, 8, 4, 2};
+        for(int num:nums)
+            bst.add(num);
+        bst.postOrder();
+        System.out.println();
+        bst.inOrder();
+        System.out.println();
+        bst.preOrder();
+
+
+
+
     }
 }
