22e8ce62cf9bf7fcf025f910b3b62db3775ba422
setsura3 <setsura3@gmail.com>
2020-02-02 16:26:38 -0800

    Update BST.java

--- src/com/company/BST.java
+++ src/com/company/BST.java
@@ -1,10 +1,12 @@
 package com.company;
-
+import java.util.Stack;
 public class BST<E extends Comparable<E>> {
-    private class Node{
+
+    private class Node {
         public E e;
         public Node left, right;
-        public Node(E e){
+
+        public Node(E e) {
             this.e = e;
             left = null;
             right = null;
@@ -13,16 +15,86 @@ public class BST<E extends Comparable<E>> {
 
     private Node root;
     private int size;
+
     public BST(){
         root = null;
         size = 0;
     }
-
+    //get the size of BST
     public int size(){
         return size;
     }
-
+    //check if BST is empty
     public boolean isEmpty(){
         return size == 0;
+    }
+    //add an element to BST
+    public void add(E e){
+        root = add(root, e);
+    }
+
+    private Node add(Node node, E e){
+        if(node == null){
+            size ++;
+            return new Node(e);
+        }
+
+        if(e.compareTo(node.e) < 0)
+            node.left = add(node.left, e);
+        else if(e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
+    }
+    //check if a BST contains an element
+    public boolean contains(E e){
+        return contains(root, e);
+    }
+    private boolean contains(Node node, E e){
+
+        if(node == null)
+            return false;
+
+        if(e.compareTo(node.e) == 0)
+            return true;
+        else if(e.compareTo(node.e) < 0)
+            return contains(node.left, e);
+        else // e.compareTo(node.e) > 0
+            return contains(node.right, e);
+    }
+
+    public void preOrder(){
+        preOrder(root);
+    }
+    private void preOrder(Node node){
+        if(node == null)
+            return;
+
+        System.out.print(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void postOrder(){
+        postOrder(root);
+    }
+    private void postOrder(Node node){
+        if(node == null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.e);
+    }
+
+    public void inOrder(){
+        inOrder(root);
+    }
+    private void inOrder(Node node){
+        if(node == null)
+            return;
+
+        inOrder(node.left);
+        System.out.print(node.e);
+        inOrder(node.right);
     }
 }
