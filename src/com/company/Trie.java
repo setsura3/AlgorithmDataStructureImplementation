3cacbd843a5ae022c39a2915718a1846590cad5e
Mini-Project-MovingCircles <setsura3@gmail.com>
2020-02-06 06:28:49 -0800

    Merge remote-tracking branch 'origin/master'

--- src/com/company/Trie.java
+++ src/com/company/Trie.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 public class Trie {
     private class Node {
         public boolean isWord;
-        public TreeMap<Character, Node> Next;
+        public TreeMap<Character, Node> next;
 
         public Node(boolean isWord) {
             this.isWord = isWord;
@@ -24,4 +24,33 @@ public class Trie {
         root = new Node();
         size = 0;
     }
+
+    //add a word to Trie
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+                cur = cur.next.get(c);
+            }
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    public boolean contains(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
 }
